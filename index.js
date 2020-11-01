6f60756c6d0280066df6882fd5cd3d8294466930
KsPsD <ui9621@naver.com>
2020-11-01 18:08:39 +0900

    protect

--- index.js
+++ index.js
@@ -2,15 +2,44 @@ const express = require('express')
 
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser")
+const { User } = require("./models/User")
+const config = require('./config/key')
+
+
+
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//application/json
+app.use(bodyParser.json())
+
 
 
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://castle:castle1234@boiler-plate.mropl.mongodb.net/boiler-plate?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err))
 
+
+
+
+
 app.get('/', (req, res) => res.send("hello world"))
+
+
+app.post('/register', (req, res) => {
+    //회원가입 필요한 정보들을 클라이언트에서 가져오면 db에 넣어줌
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => console.log(`example app listening on port ${port}`))
